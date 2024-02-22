896f00da4edeb06ae946fa9d1f848f7b0dd3372b
antasth <antasth@mail.ru>
2024-02-22 14:08:15 +0300

    feat: add receiving userName from query parameters

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+import crypto from 'crypto'
 import http from 'http'
+import url from 'url'
 import { WebSocketServer } from 'ws'
 
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
 
 const port = 8000
+
+wsServer.on('connection', (connection, request) => {
+  const { username } = url.parse(request.url, true).query
+  const id = crypto.randomBytes(16).toString('hex')
+
+  console.log(username, id)
+})
 
 server.listen(port, () => {
   console.log(`WS server is running on port ${port}`)
