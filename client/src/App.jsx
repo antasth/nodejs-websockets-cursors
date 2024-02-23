368e08c5128cb9b0c3f3dc6fe0421d7d3ab71832
antasth <antasth@mail.ru>
2024-02-23 18:21:43 +0300

    feat: add Home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import { Login } from './components/Login'
+import { Home } from './Home'
 
 function App() {
+  const [username, setUsername] = useState('')
+
+  if (username) {
+    return <Home username={username} />
+  }
+
   return (
     <>
       <h1>Cursors App</h1>
-      <Login />
+      <Login onSubmit={setUsername} />
     </>
   )
 }
---
+++ client/src/Home.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/prop-types
+const Home = ({ username }) => {
+  return (
+    <>
+      <h1>Home</h1>
+      <h3>Hello {username}</h3>
+    </>
+  )
+}
+
+export { Home }
